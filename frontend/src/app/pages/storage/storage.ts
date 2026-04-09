9d11b64e065cb6d6902d00238a8f86cbe9ce7f47
adam026 <adamtoth4@gmail.com>
2026-04-09 18:51:41 +0200

    Add Angular frontend

---
+++ frontend/src/app/pages/storage/storage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-storage',
+  imports: [],
+  templateUrl: './storage.html',
+  styleUrl: './storage.scss',
+})
+export class Storage {}
