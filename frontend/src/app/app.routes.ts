1dfdb42cd3f546c1aacbea053b11f53d36afd1fe
adam026 <adamtoth4@gmail.com>
2026-04-09 19:38:31 +0200

    Add initial routing and base html messages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Storage } from './pages/storage/storage';
+import { ShoppingList } from './pages/shopping-list/shopping-list';
+import { Recipes } from './pages/recipes/recipes';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'storage', component: Storage },
+  { path: 'shopping-list', component: ShoppingList },
+  { path: 'recipes', component: Recipes },
+  { path: 'about', component: About }
+];
--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, MatToolbarModule, MatButtonModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  styleUrl: './navbar.scss'
 })
+
 export class Navbar {}
